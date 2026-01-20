44fc58be0838429cd6ed49382e5567105da1f2b8
Michael de Radobýl <mjohne@users.noreply.github.com>
2026-01-21 00:30:04 +0100

    Move AssemblyInfo.cs to the directory Helpers

---
+++ Helpers/AssemblyInfo.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Planetoid_DB
+{
+	/// <summary>
+	/// Provide some assembly information
+	/// </summary>
+	public static class AssemblyInfo
+	{
+		#region Assembly attribute accessors
+
+		/// <summary>
+		/// Return the title of the assembly
+		/// </summary>
+		public static string? AssemblyTitle
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly()
+					.GetCustomAttributes(attributeType: typeof(AssemblyTitleAttribute), inherit: false);
+				return attributes.Length > 0 && attributes[0] is AssemblyTitleAttribute titleAttribute
+					? !string.IsNullOrEmpty(value: titleAttribute.Title) ? titleAttribute.Title : Path.GetFileNameWithoutExtension(path: AppContext.BaseDirectory)
+					: Path.GetFileNameWithoutExtension(path: AppContext.BaseDirectory);
+			}
+		}
+
+		/// <summary>
+		/// Return the version of the assembly
+		/// </summary>
+		public static string AssemblyVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown Version";
+
+		/// <summary>
+		/// Return the description of the assembly
+		/// </summary>
+		public static string AssemblyDescription
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly()
+					.GetCustomAttributes(attributeType: typeof(AssemblyDescriptionAttribute), inherit: false);
+				return attributes.Length == 0 ? string.Empty : ((AssemblyDescriptionAttribute)attributes[0]).Description;
+			}
+		}
+
+		/// <summary>
+		/// Return the product name of the assembly
+		/// </summary>
+		public static string AssemblyProduct
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly()
+					.GetCustomAttributes(attributeType: typeof(AssemblyProductAttribute), inherit: false);
+				return attributes.Length == 0 ? string.Empty : ((AssemblyProductAttribute)attributes[0]).Product;
+			}
+		}
+
+		/// <summary>
+		/// Return the copyright of the assembly
+		/// </summary>
+		public static string AssemblyCopyright
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly()
+					.GetCustomAttributes(attributeType: typeof(AssemblyCopyrightAttribute), inherit: false);
+				return attributes.Length == 0 ? string.Empty : ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+			}
+		}
+
+		/// <summary>
+		/// Return the company name of the assembly
+		/// </summary>
+		public static string AssemblyCompany
+		{
+			get
+			{
+				object[] attributes = Assembly.GetExecutingAssembly()
+					.GetCustomAttributes(attributeType: typeof(AssemblyCompanyAttribute), inherit: false);
+				return attributes.Length == 0 ? string.Empty : ((AssemblyCompanyAttribute)attributes[0]).Company;
+			}
+		}
+		#endregion
+	}
+}
