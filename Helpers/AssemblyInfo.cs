481dec3e46f19ba52b256f319b6678cdfedf3f91
Michael de Radobýl <mj-programming@gmx.de>
2026-03-19 21:58:59 +0100

    Enhance AssemblyInfo with NLog for error handling
    Added logging for assembly information retrieval methods to handle exceptions gracefully.

--- Helpers/AssemblyInfo.cs
+++ Helpers/AssemblyInfo.cs
@@ -1,7 +1,9 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
+// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
+
+using NLog;
 
 using System.Reflection;
 
@@ -11,6 +13,10 @@ namespace Planetoid_DB.Helpers;
 /// <remarks>This class provides access to assembly-level attributes.</remarks>
 public static class AssemblyInfo
 {
+	/// <summary>NLog logger instance.</summary>
+	/// <remarks>This logger is used to log messages and errors for the class.</remarks>
+	private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+	
 	#region Assembly attribute accessors
 
 	/// <summary>Return the title of the assembly</summary>
@@ -19,17 +25,39 @@ public static class AssemblyInfo
 	{
 		get
 		{
-			object[] attributes = Assembly.GetExecutingAssembly()
-				.GetCustomAttributes(attributeType: typeof(AssemblyTitleAttribute), inherit: false);
-			return attributes.Length > 0 && attributes[0] is AssemblyTitleAttribute titleAttribute
-				? !string.IsNullOrEmpty(value: titleAttribute.Title) ? titleAttribute.Title : Path.GetFileNameWithoutExtension(path: AppContext.BaseDirectory)
-				: Path.GetFileNameWithoutExtension(path: AppContext.BaseDirectory);
+			try
+			{
+				object[] attributes = Assembly.GetExecutingAssembly()
+					.GetCustomAttributes(attributeType: typeof(AssemblyTitleAttribute), inherit: false);
+				return attributes.Length > 0 && attributes[0] is AssemblyTitleAttribute titleAttribute
+					? !string.IsNullOrEmpty(value: titleAttribute.Title) ? titleAttribute.Title : Path.GetFileNameWithoutExtension(path: Assembly.GetExecutingAssembly().Location)
+					: Path.GetFileNameWithoutExtension(path: Assembly.GetExecutingAssembly().Location);
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(exception: ex, message: "An error occurred while retrieving the assembly title.");
+				return string.Empty;
+			}
 		}
 	}
 
 	/// <summary>Return the version of the assembly</summary>
 	/// <remarks>This property retrieves the version of the assembly.</remarks>
-	public static string AssemblyVersion => Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "Unknown Version";
+	public static string AssemblyVersion
+	{
+		get
+		{
+			try
+			{
+				return Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? string.Empty;
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(exception: ex, message: "An error occurred while retrieving the assembly version.");
+				return string.Empty;
+			}
+		}
+	}
 
 	/// <summary>Return the description of the assembly</summary>
 	/// <remarks>This property retrieves the description of the assembly.</remarks>
@@ -37,9 +65,16 @@ public static class AssemblyInfo
 	{
 		get
 		{
-			object[] attributes = Assembly.GetExecutingAssembly()
-				.GetCustomAttributes(attributeType: typeof(AssemblyDescriptionAttribute), inherit: false);
-			return attributes.Length == 0 ? string.Empty : ((AssemblyDescriptionAttribute)attributes[0]).Description;
+			try
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(attributeType: typeof(AssemblyDescriptionAttribute), inherit: false);
+				return attributes.Length == 0 ? string.Empty : ((AssemblyDescriptionAttribute)attributes[0]).Description;
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(exception: ex, message: "An error occurred while retrieving the assembly description.");
+				return string.Empty;
+			}
 		}
 	}
 
@@ -49,9 +84,16 @@ public static class AssemblyInfo
 	{
 		get
 		{
-			object[] attributes = Assembly.GetExecutingAssembly()
-				.GetCustomAttributes(attributeType: typeof(AssemblyProductAttribute), inherit: false);
-			return attributes.Length == 0 ? string.Empty : ((AssemblyProductAttribute)attributes[0]).Product;
+			try
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(attributeType: typeof(AssemblyProductAttribute), inherit: false);
+				return attributes.Length == 0 ? string.Empty : ((AssemblyProductAttribute)attributes[0]).Product;
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(exception: ex, message: "An error occurred while retrieving the assembly product.");
+				return string.Empty;
+			}
 		}
 	}
 
@@ -61,9 +103,16 @@ public static class AssemblyInfo
 	{
 		get
 		{
-			object[] attributes = Assembly.GetExecutingAssembly()
-				.GetCustomAttributes(attributeType: typeof(AssemblyCopyrightAttribute), inherit: false);
-			return attributes.Length == 0 ? string.Empty : ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+			try
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(attributeType: typeof(AssemblyCopyrightAttribute), inherit: false);
+				return attributes.Length == 0 ? string.Empty : ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(exception: ex, message: "An error occurred while retrieving the assembly copyright.");
+				return string.Empty;
+			}
 		}
 	}
 
@@ -73,9 +122,16 @@ public static class AssemblyInfo
 	{
 		get
 		{
-			object[] attributes = Assembly.GetExecutingAssembly()
-				.GetCustomAttributes(attributeType: typeof(AssemblyCompanyAttribute), inherit: false);
-			return attributes.Length == 0 ? string.Empty : ((AssemblyCompanyAttribute)attributes[0]).Company;
+			try
+			{
+				object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(attributeType: typeof(AssemblyCompanyAttribute), inherit: false);
+				return attributes.Length == 0 ? string.Empty : ((AssemblyCompanyAttribute)attributes[0]).Company;
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(exception: ex, message: "An error occurred while retrieving the assembly company.");
+				return string.Empty;
+			}
 		}
 	}
 	#endregion
