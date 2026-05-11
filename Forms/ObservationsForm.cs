5750985b6310b5df80fe6db5a46f08b08b22096b
Michael de Radobýl <mj-programming@gmx.de>
2026-05-11 17:32:44 +0200

    Bugfixed `ObservationsForm`

--- Forms/ObservationsForm.cs
+++ Forms/ObservationsForm.cs
@@ -3,6 +3,8 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
+using Krypton.Toolkit;
+
 using NLog;
 
 using Planetoid_DB.Forms;
