695e4a7c459bba38d9a367321628d33e162d30ad
Michael de Radobýl <mj-programming@gmx.de>
2026-09-22 22:38:44 +0200

    Add `DerivedElements.OrbitalResonance`

--- Helpers/DerivedElements.cs
+++ Helpers/DerivedElements.cs
@@ -21,7 +21,7 @@ namespace Planetoid_DB.Helpers;
 /// <remarks>This class contains methods for calculating the semi-minor axis, linear eccentricity, major axis, minor axis, and other orbital elements.</remarks>
 // You can customize the debugger display for this class by providing a property that returns a string representation of the instance, which will be shown in the debugger when you inspect an object of this class. In this case, the DebuggerDisplay property is used to return a string representation of the instance, and the DebuggerDisplay attribute is applied to the class to specify that this property should be used for the debugger display.
 [DebuggerDisplay(value: $"{{{nameof(DebuggerDisplay)},nq}}")]
-internal class DerivedElements
+internal partial class DerivedElements
 {
 	/// <summary>Represents the standard gravitational parameter (GM) for the Sun in AU^3/year^2.</summary>
 	/// <remarks>The standard gravitational parameter is used in various orbital calculations.</remarks>
@@ -305,27 +305,6 @@ internal class DerivedElements
 		// The standard gravitational parameter (GM) can be calculated using Kepler's third law: GM = 4π²a³/T², where a is the semi-major axis and T is the orbital period.
 		double period = CalculatePeriod(semiMajorAxis: semiMajorAxis);
 		return gm * Math.Pow(x: semiMajorAxis, y: 3) / (period * period);
-	}
-
-	/// <summary>Represents an orbital resonance between a planetoid and a solar system planet.</summary>
-	/// <param name="PlanetName">The name of the planet.</param>
-	/// <param name="PlanetPeriod">The orbital period of the planet in years.</param>
-	/// <param name="PlanetoidPeriod">The orbital period of the planetoid in years.</param>
-	/// <param name="Ratio">The actual ratio of the planet's period to the planetoid's period.</param>
-	/// <param name="ResonanceP">The p value in the integer resonance ratio p:q.</param>
-	/// <param name="ResonanceQ">The q value in the integer resonance ratio p:q.</param>
-	/// <param name="DeviationPercent">The percentage deviation of the actual ratio from the integer ratio.</param>
-	/// <remarks>This record is used to represent an orbital resonance between a planetoid and a solar system planet.</remarks>
-	[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
-	internal record OrbitalResonance(string PlanetName, double PlanetPeriod, double PlanetoidPeriod, double Ratio, int ResonanceP, int ResonanceQ, double DeviationPercent)
-	{
-		/// <summary>Returns a string representation of the orbital resonance for debugging purposes.</summary>
-		/// <returns>A string representation of the orbital resonance.</returns>
-		/// <remarks>This method is used to provide a human-readable representation of the orbital resonance for debugging.</remarks>
-		private string GetDebuggerDisplay()
-		{
-			return ToString();
-		}
 	}
 
 	/// <summary>Calculates the orbital resonances of a planetoid with the 8 solar system planets.</summary>
